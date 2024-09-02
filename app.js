def6efa05943f9ee3e4f807811a3c7d57d127487
SudarshanKolhe07 <sudarshankolhe04@gmail.com>
2024-09-02 20:54:41 +0530

    Update changes

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get("/listings", async (req, res) => {
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    console.log(listing);
     res.render("listings/show.ejs", { listing });
 });
 
--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,10 @@ const ListingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        default: "https://unsplash.com/photos/a-group-of-people-standing-around-a-gazebo-in-the-desert-63FGF7bLQjY",
+        default: "https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) =>
             v === ""
-                ? "https://unsplash.com/photos/a-group-of-people-standing-around-a-gazebo-in-the-desert-63FGF7bLQjY"
+                ? "https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 : v,
     },
     price: Number,
